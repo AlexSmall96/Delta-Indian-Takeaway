557e05f9b3da0e206281f6e6088beffe55b0f752
Alex Small <alex.small739@gmail.com>
2025-05-28 16:33:59 +0100

    Add offcanvas to NavBar

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom/cjs/react-router-dom.min';
+import Menu from './pages/Menu';
+import HomePage from './pages/HomePage';
+import NavBar from './components/NavBar';
+import About from './pages/About';
+import Location from './pages/Location';
+import Footer from './components/Footer';
+import styles from './App.module.css'
+
+function App() {
+	return (
+		<div className={styles.App}>
+			<Router>
+				<NavBar />
+				<Switch>
+					<Route exact path='/' render={() => <HomePage />} />
+					<Route exact path='/menu' render={() => <Menu />} />
+					<Route exact path='/about' render={() => <About />} />
+					<Route exact path='/location' render={() => <Location />} />
+				</Switch>
+				<Footer />
+			</Router>
+		</div>
+	);
+}
+
+export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,26 +1,58 @@
-import React from "react"
-import { Link } from 'react-router-dom/cjs/react-router-dom';
+import React, {useEffect, useState} from 'react';
+import { Navbar, Nav, Image, NavDropdown, Offcanvas, Col, Container, Row, Card} from 'react-bootstrap'
+import appStyles from '../App.module.css'
 
 const NavBar = () => {
+
+    // Initialize state variables
+    const [expanded, setExpanded] = useState(false)
+
+    // useEffect(() => {
+    //     if (width > 767){
+    //         setExpanded(false)
+    //     }
+    // }, [width])
+
+    // Ensure nav bar is collapsed when user clicks another part of screen
+    document.addEventListener('mouseup', (event) => {
+        if (!event.target.classList.contains('offcanvas-body')){
+            setExpanded(false) 
+        }
+    })
+
+    const handleClick = () => {
+        setExpanded(false)
+    }
+
+
     return (
-        <ul>
-            <li>
-                <Link to='/'>Delta Indian Takeaway</Link>
-            </li>
-            <li>
-                <Link to='/'>Home</Link>
-            </li>
-            <li>
-                <Link to='/menu'>Menu</Link>
-            </li>
-            <li>
-                <Link to='/about'>About</Link>
-            </li>
-            <li>
-                <Link to='/location'>Location</Link>
-            </li>
-        </ul>
+        <Navbar expand={'md'} expanded={expanded} sticky='top'>
+            {/* LOGO */}
+            <Navbar.Brand href="/">
+                <h3 className={appStyles.horizMargin}>
+                    Delta Indian Takeaway
+                </h3>
+            </Navbar.Brand>
+            {/* HAMBURGER MENU TO EXPAND/COLLAPSE */}
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md`} onClick={() => setExpanded(true)}/>
+            <Navbar.Offcanvas
+                id={`offcanvasNavbar-expand-md`}
+                aria-labelledby={`offcanvasNavbarLabel-expand-md`}
+                placement="end"
+            >
+                <Offcanvas.Header closeButton>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    <Nav className={`justify-content-end flex-grow-1 pe-3`}>
+                        <Nav.Link href='/' onClick={handleClick}>Home</Nav.Link>
+                        <Nav.Link href='/menu' onClick={handleClick}>Menu</Nav.Link>
+                        <Nav.Link href='/about' onClick={handleClick}>About</Nav.Link>
+                        <Nav.Link href='/location' onClick={handleClick}>Location</Nav.Link>
+                    </Nav>
+                </Offcanvas.Body>
+            </Navbar.Offcanvas>
+      	</Navbar>
     )
-};
+}
 
 export default NavBar
