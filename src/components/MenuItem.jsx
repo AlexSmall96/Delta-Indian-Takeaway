2f3453b28d4674598ced064020c257ef79043b77
Alex Small <alex.small739@gmail.com>
2025-06-10 11:24:40 +0100

    Add check for show dots and add chilly icons

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -2,14 +2,25 @@ import React from "react"
 import styles from '../styles/MenuItem.module.css'
 
 const MenuItem = ({item}) => {
+
+    const showDots = item.category !== 'DELTA CLASSICS' || item.price
+
     return (
         <>
             <div className={styles.item}>
                 <div className={styles.itemName}>{item.name}</div>
-                <div className={styles.dotted}></div>
+                { showDots? <div className={styles.dotted}></div>: ''}
                 <div className={styles.itemPrice}>{item.price}</div>
             </div>
-            <p className={styles.itemDescription}>{item.description}</p>
+            <p className={styles.itemDescription}>{item.description} <br /></p>
+            <p className={styles.itemSpice}>
+                {item.spice? 
+                        item.spice === 2? 
+                            <><i class="fa-solid fa-pepper-hot"></i><i class="fa-solid fa-pepper-hot"></i></>
+                        : 
+                            <i class="fa-solid fa-pepper-hot"></i>:
+                ''}
+            </p>
         </>
     )
 };
