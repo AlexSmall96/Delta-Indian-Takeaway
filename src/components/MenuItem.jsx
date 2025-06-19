e6517c70a9e2e6b994bb97c8b554d0d9f53ddf8e
Alex Small <alex.small739@gmail.com>
2025-06-19 12:43:39 +0100

    Add vegetarian icon, update chilly icons for spice level 3

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -3,23 +3,29 @@ import styles from '../styles/MenuItem.module.css'
 
 const MenuItem = ({item}) => {
 
+    // Determine if dots are required on menu page
     const showDots = item.category !== 'DELTA CLASSICS' || item.price
+
+    // Spice levels to determine number of chilly icons to show
+    const spiceLevels = [1, 2, 3]
 
     return (
         <>
+            {/* ITEM NAME, DESCRIPTION & PRICE */}
             <div className={styles.item}>
                 <div className={styles.itemName}>{item.name}</div>
                 { showDots? <div className={styles.dotted}></div>: ''}
                 <div className={styles.itemPrice}>{item.price}</div>
             </div>
             <p className={styles.itemDescription}>{item.description} <br /></p>
-            <p className={styles.itemSpice}>
-                {item.spice? 
-                        item.spice === 2? 
-                            <><i class="fa-solid fa-pepper-hot"></i><i class="fa-solid fa-pepper-hot"></i></>
-                        : 
-                            <i class="fa-solid fa-pepper-hot"></i>:
-                ''}
+            {/* CHILLY & VG ICONS  */}
+            <p className={styles.icons}>
+                {item.spice?
+                    <span className={styles.spice}>{spiceLevels.map(level => item.spice >= level ? <i class="fa-solid fa-pepper-hot"></i>: '' )}</span>
+                :''}
+                {item.vegetarian? 
+                    <span className={styles.vegetarian}>VG</span>
+                :''}
             </p>
         </>
     )
