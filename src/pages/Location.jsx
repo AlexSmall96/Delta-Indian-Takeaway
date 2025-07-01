65304151d5ff4b252744d95e4418a6bf651927ec
Alex Small <alex.small739@gmail.com>
2025-07-01 16:08:56 +0100

    Add text and google maps background image

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -1,10 +1,37 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import styles from '../styles/Location.module.css'
 
 const Location = () => {
-    return (
-        <div>
-          Location
-        </div>
+	
+	// Initialize map url variable
+	const [mapUrl, setMapUrl] = useState('')
+
+	// Fetch static google map for background image
+	useEffect(() => {
+		const fetchMap = async () => {
+			try {
+				const response = await fetch(`http://localhost:3001/map`)
+				const blob = await response.blob();
+        		const objectUrl = URL.createObjectURL(blob);
+        		setMapUrl(objectUrl);
+			} catch (err) {
+				console.log(err)
+			}
+		}
+		fetchMap()
+	}, [])
+
+	return (
+		<>	
+			{/* INFORMATION ON LOCATION */}
+			<div className={styles.locationText}>
+				<h4>Come Visit Us</h4>
+				<p>We are located just steps away from the famous Murrayfield and Roseburn bars as well as a short walk away from Edinburgh's Murrayfield Stadium - the home of Scottish Rugby.</p>
+			</div>
+			{/* STATIC GOOGLE MAP AS BACKGROUND IMAGE */}
+			<div className={styles.backgroundMap} style={{backgroundImage: `url('${mapUrl}')`}}>
+			</div>		
+		</>
     )
 };
 
