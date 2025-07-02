077ce24c7266e9d903d51feb6d6742120e5a578b
Alex Small <alex.small739@gmail.com>
2025-07-02 15:03:23 +0100

    Add loading spinner to map

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"
 import styles from '../styles/Location.module.css'
-import { ButtonGroup, Button } from "react-bootstrap";
+import { ButtonGroup, Button, Spinner } from "react-bootstrap";
 import { motion } from 'framer-motion';
 
 const Location = () => {
@@ -8,6 +8,7 @@ const Location = () => {
 	// Initialize map variables
 	const [mapUrl, setMapUrl] = useState('')
 	const [type, setType] = useState('roadmap')
+	const [hasLoaded, setHasLoaded] = useState(false)
 
 	// Fetch static google map for background image
 	useEffect(() => {
@@ -17,6 +18,7 @@ const Location = () => {
 				const blob = await response.blob();
         		const objectUrl = URL.createObjectURL(blob);
         		setMapUrl(objectUrl);
+				setHasLoaded(true)
 			} catch (err) {
 				console.log(err)
 			}
@@ -39,15 +41,20 @@ const Location = () => {
 				<p>We are located just steps away from the famous Murrayfield and Roseburn bars as well as a short walk away from Edinburgh's Murrayfield Stadium - the home of Scottish Rugby.</p>
 			</div>
 			{/* STATIC GOOGLE MAP AS BACKGROUND IMAGE */}
-			<div className={styles.backgroundMap} style={{backgroundImage: `url('${mapUrl}')`}}>
-				<ButtonGroup className={styles.toggleMap}>
-					<Button variant="light" onClick={() => setType('roadmap')}>Map</Button>
-					<Button variant="light" onClick={() => setType('hybrid')}>Satellite</Button>
-				</ButtonGroup>
-				<Button className={styles.directions} variant='light'>
-					<a target='_blank' rel="noreferrer" href='https://www.google.com/maps/dir//27+Roseburn+Terrace,+Edinburgh+EH12+5NG/@55.9484367,-3.2367662,16z/data=!4m8!4m7!1m0!1m5!1m1!1s0x4887c7accedae949:0xf44a5da22899ca4e!2m2!1d-3.2344121!2d55.9454082?entry=ttu&g_ep=EgoyMDI1MDYyOS4wIKXMDSoASAFQAw%3D%3D'>Directions</a>
-				</Button>
-			</div>		
+			{hasLoaded ?
+				<div className={styles.backgroundMap} style={{backgroundImage: `url('${mapUrl}')`}}>
+					<ButtonGroup className={styles.toggleMap}>
+						<Button variant="light" onClick={() => setType('roadmap')}>Map</Button>
+						<Button variant="light" onClick={() => setType('hybrid')}>Satellite</Button>
+					</ButtonGroup>
+					<Button className={styles.directions} variant='light'>
+						<a target='_blank' rel="noreferrer" href='https://www.google.com/maps/dir//27+Roseburn+Terrace,+Edinburgh+EH12+5NG/@55.9484367,-3.2367662,16z/data=!4m8!4m7!1m0!1m5!1m1!1s0x4887c7accedae949:0xf44a5da22899ca4e!2m2!1d-3.2344121!2d55.9454082?entry=ttu&g_ep=EgoyMDI1MDYyOS4wIKXMDSoASAFQAw%3D%3D'>Directions</a>
+					</Button>
+				</div>	
+			:	
+				<div className={styles.spinnerParent}>
+					<Spinner />
+				</div>}	
 		</motion.div>
     )
 };
